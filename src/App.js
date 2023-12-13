d915f772bcc42d82e66298423e14d0f743832e69
Keerthana B <bkeerthanaeee@gmail.com>
2023-12-13 15:11:50 +0530

    13.12.23 updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import InputArea from "./components/InputArea"
 
 function App() {
   return (
     <div className="App">
     <h1>Project:4 Shopping List</h1>
+    <InputArea/>
  </div>
   );
 }
