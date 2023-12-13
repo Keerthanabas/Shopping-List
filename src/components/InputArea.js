cc9a0d7370dfef455603e735625404260c273e0d
Keerthana B <bkeerthanaeee@gmail.com>
2023-12-13 19:21:02 +0530

    adding usestate

--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const InputArea = () => {
+    const [inputText,setInputText] = useState("")
+
+    const handleChange = (event) =>{
+        setInputText(event.target.value)
+    }
     return (
         <div>
             <h2>Items To Buy</h2>
-            <input type="text" placeholder="Add new item"></input>
+            <input type="text" placeholder="Add new item" onChange={handleChange} value={inputText}></input>
             <button>Add</button>
         </div>
 
