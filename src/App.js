fa89e5be14594711f3bc1a6fc73ea721e4d14e84
Keerthana B <bkeerthanaeee@gmail.com>
2023-12-14 18:53:03 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import InputArea from "./components/InputArea"
 
 function App() {
+
+  const [items,setItems] = useState ([])
+
+  const addItems = (inputText) =>{
+    setItems((prevItems) => {
+      return [...prevItems,inputText]
+})
+}
   
   return (
     <div className="App">
     <h1>Project:3 Shopping List</h1>
-    <InputArea/>
+    <InputArea additems = {addItems}/>
  </div>
   );
 }
