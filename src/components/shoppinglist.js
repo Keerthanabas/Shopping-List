31768b4c986b2ee4111e6af580eecd1bbc8c598d
Keerthana B <bkeerthanaeee@gmail.com>
2024-01-02 17:59:37 +0530

    2.1.24 changes

--- src/components/shoppinglist.js
+++ src/components/shoppinglist.js
@@ -4,7 +4,7 @@ import React from 'react';
 const ShoppingList = (props)=>{
     return(
         <div>
-        <li>{props.text}</li> 
+        <li className="list">{props.text}</li> 
         {/* <button onClick={()=>props.deleteItem(props.id)}> </button> */}
         <span class="material-symbols-outlined" onClick={()=>props.deleteItem(props.id)}> Cancel</span> 
         </div>
